85c89749a433051861364329793b3e543cfdfbb2
Suraj Kumar <76599223+surajkumar@users.noreply.github.com>
2022-10-17 20:19:06 +0100

    Updates

--- frontend/src/main/java/dtfb/controller/ChannelController.java
+++ frontend/src/main/java/dtfb/controller/ChannelController.java
@@ -1,7 +1,6 @@
 package dtfb.controller;
 
 import dtfb.entity.CmChannel;
-import dtfb.entity.ServerStructure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
